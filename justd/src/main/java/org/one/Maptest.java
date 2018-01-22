cc17e80a167090f658614803209603e0a6a46f97
justd <IsZhangjd@outlook.com>
2018-01-23 00:10:11 +0800

    map

--- justd/src/main/java/org/one/Maptest.java
+++ justd/src/main/java/org/one/Maptest.java
@@ -1,9 +1,27 @@
 package org.one;
 
-import org.json.JSONArray;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Maptest {
     public static void main(String[] args) {
-        JSONArray
+        JSONArray ja= new JSONArray();
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("个人",String.valueOf(1));
+        jsonObject.put("geren2",String.valueOf(2));
+        JSONObject js = new JSONObject();
+        js.put("tuandui",String.valueOf(3));
+        js.put("tuamdio2",String.valueOf(4));
+        JSONObject jss = new JSONObject();
+        jss.put("1",jsonObject);
+        jss.put("2",js);
+        HashMap hm = new HashMap();
+        hm.put("geren",jsonObject);
+        hm.put("22",js);
+        System.out.println(jss);
     }
 }
